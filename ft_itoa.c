173b826b698cdbcc71a0bdb299184455333b7a28
Léonard Marques <lmarques@student.42.fr>
2016-09-14 03:42:03 +0200

    ITOA DONE ! :D

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,96 +11,50 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
-/*
-static	int	ft_slen(const char *s)
-{
-	int	count;
-
-	count = 0;
-	while (s[count])
-		count++;
-	return (count);
-}
-
-static char	*ft_reverse(char *str, size_t len)
-{
-	int		count;
-	int		end;
-	char	temp;
-
-	count = 0;
-	end = len - 1;
-	while (count < end)
-	{
-		temp = str[count];
-		str[count] = str[end];
-		str[end] = str[temp];
-		count++;
-		end--;
-	}
-}
 
 static int	ft_count_nb(int nb)
 {
-	int	count;
+	int			count;
+	long int	long_nb;
 
 	count = 0;
-	if (nb >= -9 && nb <= 9)
-		return (1);
+	long_nb = nb;
 	if (nb < 0)
 	{
-		nb = -nb;
+		long_nb = -nb;
 		count++;
 	}
-	while (nb > 1)
+	while (long_nb != long_nb % 10)
 	{
-		nb /= 10;
+		long_nb = (long_nb - long_nb % 10) / 10;
 		count++;
 	}
+	count++;
 	return (count);
 }
-*/
+
 char	*ft_itoa(int nb)
 {
-/*
-	int		count;
-	int		tmp;
-	int		is_negative;
-	char	*str;
+	char		*str;
+	int			count;
+	long int	long_nb;
 
 	count = 0;
-	tmp = 0;
-	is_negative = 0;
-	str = 0;
-	str = (char *)malloc(sizeof(char) * ft_count_nb(nb) + 2);
-	if (str)
+	long_nb = nb;
+	str = (char *)malloc(sizeof(char) * (ft_count_nb(nb) + 1));
+	if (!str)
+		return (NULL);
+	if (nb < 0)
 	{
-		if (nb == 0)
-			return ("0");
-		if (nb < 0)
-		{
-			nb = -nb;
-			is_negative = 1;
-		}
-		while (nb != 0)
-		{
-			tmp = nb % 10;
-			if (tmp > 9)
-				str[count] = (tmp - 10) + 'a';
-			else
-				str[count] = tmp + '0';
-			count++;
-			nb /= 10;
-		}
-		if (is_negative == 1)
-			str[count++] = '-';
-		str[count] = '\0';
-		str = ft_reverse(str, count);
+		str[0] = '-';
+		long_nb *= -1;
 	}
+	while (count < (nb < 0 ? ft_count_nb(nb) - 1 : ft_count_nb(nb)))
+	{
+		str[ft_count_nb(nb) - count - 1] = (long_nb % 10) + '0';
+		long_nb = (long_nb - long_nb % 10) / 10;
+		count++;
+	}
+	str[ft_count_nb(nb)] = '\0';
 	return (str);
-*/
-	nb++;
-	return ("0");
 }
