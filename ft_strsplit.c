73b97acf7608926f29f790538ba8aac2de59662c
Léonard Marques <lmarques@student.42.fr>
2016-09-14 23:32:28 +0200

    corrected ft_strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/30 23:12:09 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/10 02:00:43 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/09/14 22:30:16 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ static int	ft_nbwords(char const *str, char c)
 	count_words = 0;
 	while (str[count_str])
 	{
-		if (str[count_str] != c && (str[count_str] == 0 || str[count_str] == c))
+		if (str[count_str] != c && (count_str == 0 ||
+			str[count_str - 1] == c))
 			count_words++;
 		count_str++;
 	}
@@ -68,6 +69,8 @@ char	**ft_strsplit(char const *s, char c)
 	count_s = 0;
 	count_str = 0;
 	str = NULL;
+	if (!s)
+		return (NULL);
 	str = (char **)malloc(sizeof(str) * (ft_nbwords(s, c)) + 1);
 	if (str)
 	{
@@ -84,3 +87,20 @@ char	**ft_strsplit(char const *s, char c)
 	}
 	return (str);
 }
+/*
+int	main(int argc, char *argv[])
+{
+	char	**tab;
+	int		count;
+
+	tab = ft_strsplit(argv[1], argv[2][0]);
+	count = 0;
+	while (tab[count])
+	{
+		printf("%s\n", tab[count]);
+		count++;
+	}
+	argc++;
+	return (0);
+}
+*/
