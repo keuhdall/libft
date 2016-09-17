c19d381ea05b27ccdf6123e84de1a2d024fd448d
Léonard Marques <lmarques@student.42.fr>
2016-09-17 21:44:16 +0200

    added unsigned char cast

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/06 05:33:05 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/02 17:40:27 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/09/17 21:27:37 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	char	*str;
-	size_t	count;
+	unsigned char	*str;
+	size_t			count;
 
-	str = (char *)b;
+	if (!b)
+		return (NULL);
+	str = (unsigned char *)b;
 	count = 0;
 	while (count < len)
 	{
-		str[count] = c;
+		str[count] = (unsigned char)c;
 		count++;
 	}
 	return (b);
