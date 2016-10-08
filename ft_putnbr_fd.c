8e8c0a34bb06ebbc76167783060bc698022147b0
Léonard Marques <lmarques@student.42.fr>
2016-10-08 16:34:11 +0200

    corrected ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,18 +6,13 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/30 22:47:16 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/20 14:45:06 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/10/08 16:33:07 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_pchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
-
-void		ft_putnbr_fd(int nb, int fd)
+void	ft_putnbr_fd(int nb, int fd)
 {
 	long int	long_nb;
 
@@ -25,9 +20,9 @@ void		ft_putnbr_fd(int nb, int fd)
 	if (long_nb < 0)
 	{
 		long_nb = -long_nb;
-		ft_pchar_fd('-', fd);
+		ft_putchar_fd('-', fd);
 	}
 	if (long_nb > 9)
-		ft_putnbr_fd(long_nb / 10, 1);
-	ft_pchar_fd(long_nb % 10 + '0', fd);
+		ft_putnbr_fd(long_nb / 10, fd);
+	ft_putchar_fd(long_nb % 10 + '0', fd);
 }
