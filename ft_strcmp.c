8f532441056e613090227e927ee7de5dea52a414
Léonard Marques <lmarques@student.42.fr>
2016-10-09 02:53:46 +0200

    added unsigned char cast to the returned value

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/05 05:01:21 by lmarques          #+#    #+#             */
-/*   Updated: 2016/10/08 18:41:28 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/10/09 02:51:13 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int	ft_strcmp(const char *s1, const char *s2)
 	count = 0;
 	while (s1[count] && s2[count] && s1[count] == s2[count])
 		count++;
-	return ((unsigned char)s1[count] - s2[count]);
+	return ((unsigned char)s1[count] - (unsigned char)s2[count]);
 }
