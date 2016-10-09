a17bf36c789a167238c09f6d0eeb0b7a84435405
Léonard Marques <lmarques@student.42.fr>
2016-10-09 02:48:59 +0200

    corrected the size of the malloc

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/04 05:16:39 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/09 04:09:29 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/10/09 02:46:26 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strdup(const char *s1)
 
 	count = 0;
 	str2 = NULL;
-	str2 = (char *)malloc(sizeof(s1) * ft_strlen(s1) + 1);
+	str2 = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
 	if (str2)
 	{
 		while (s1[count])
