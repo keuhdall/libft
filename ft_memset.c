ddba179e2371f53e7998c9538180f0879db16a39
Léonard Marques <lmarques@student.42.fr>
2016-10-12 16:51:14 +0200

    unprotected the function so it mimics the exact behavior of the function

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/06 05:33:05 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/17 21:27:37 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/10/12 16:44:29 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memset(void *b, int c, size_t len)
 	unsigned char	*str;
 	size_t			count;
 
-	if (!b)
-		return (NULL);
 	str = (unsigned char *)b;
 	count = 0;
 	while (count < len)
