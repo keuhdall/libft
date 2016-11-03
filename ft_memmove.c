29dc3a9e510f5aeeae406d9245873c53554ade1c
keuhdall <lmarques@student.42.fr>
2016-11-03 12:26:49 +0100

    fixed ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/06 18:15:48 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/11 17:02:13 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/03 12:25:28 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,13 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	char	tmp[n];
-	size_t	count;
-
-	count = 0;
-	while (count < n)
+	if (dst <= src)
+		return (ft_memcpy(dst, src, n));
+	while (n > 0)
 	{
-		((char *)tmp)[count] = ((char *)src)[count];
-		count++;
+		((char *)dst)[n] = ((char *)src)[n];
+		n--;
 	}
-	count = 0;
-	while (count < n)
-	{
-		((char *)dst)[count] = tmp[count];
-		count++;
-	}
+	((char *)dst)[n] = ((char *)src)[n];
 	return (dst);
 }
