736d5dae2a28e484f5f1f06972e1f4e43c5018c4
keuhdall <lmarques@student.42.fr>
2016-11-03 17:19:39 +0100

    corrected memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/06 18:15:48 by lmarques          #+#    #+#             */
-/*   Updated: 2016/11/03 12:25:28 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/03 16:29:46 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
+	if (n == 0)
+		return (dst);
 	if (dst <= src)
 		return (ft_memcpy(dst, src, n));
+	n--;
 	while (n > 0)
 	{
 		((char *)dst)[n] = ((char *)src)[n];
