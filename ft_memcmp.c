5814828025960a0fa7b29e53f5d8ad5bdb20cfa1
keuhdall <lmarques@student.42.fr>
2016-11-03 11:39:36 +0100

    corrected ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/06 21:51:32 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/03 21:20:49 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/03 11:38:08 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,10 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	char	*str_s2;
 	size_t	count;
 
-	if (!s1 || !s2)
-		return (0);
 	str_s1 = (char *)s1;
 	str_s2 = (char *)s2;
 	count = 0;
-	while (str_s1[count] && str_s2[count] &&
-			str_s1[count] == str_s2[count] &&
-			count < n)
+	while (count < n && str_s1[count] == str_s2[count])
 		count++;
 	if (count == n)
 		return (0);
