f5e6fa6c38796cd013bee7f81246bf78cfb0ff77
keuhdall <lmarques@student.42.fr>
2016-11-03 09:21:13 +0100

    suppressed protection

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/14 16:33:14 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/20 14:38:32 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/03 09:18:26 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	ft_lstadd(t_list **alst, t_list *new)
 {
-	if (!alst || !*alst || !new)
-		return ;
 	new->next = *alst;
 	*alst = new;
 }
