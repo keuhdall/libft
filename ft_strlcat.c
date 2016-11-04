86370b50e7906fa9bb97e87a7eddca1a9250643c
keuhdall <lmarques@student.42.fr>
2016-11-04 16:11:05 +0100

    corrected a little mistake on strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/08 04:04:40 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/11 03:03:58 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/04 16:09:06 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 	dst_size = ft_strlen(dst);
 	count = 0;
-	if (size < dst_size)
+	if (size <= dst_size)
 		return (ft_strlen(src) + size);
 	while (src[count] && count < (size - dst_size - 1))
 	{
