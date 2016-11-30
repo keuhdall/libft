d88d1fe905f34c21b1a8d4b119a2228a3d72bea4
keuhdall <lmarques@student.42.fr>
2016-11-30 15:40:42 +0100

    corrected include path for libft.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/29 23:38:03 by lmarques          #+#    #+#             */
-/*   Updated: 2016/11/21 14:46:55 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/30 15:39:22 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 1
 
-# include "./libft/libft.h"
+# include "./libft.h"
 # include <fcntl.h>
 
 typedef struct		s_data
